8a9a95667d8cfb48752d60fe919634d83faaf4a5
amelieplenio <aplenio2@illinois.edu>
2026-05-10 22:48:41 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
+def make_scores():
+    return{
+        "openness": 0,
+        "extraversion": 0,
+        "agreeableness": 0,
+        "conscientousness": 0,
+        "neuroticism": 0, }
+    
 def play_scenario (scenario):
-    """Display a scenario and get users choice."""
+    print("/n" + scenario[title"])
+    print(scenario["text]")
+
+    for number, option in enumerate(scenario["options"], start=1):
+    while True
+        choice = input("Choose 1, 2, or 3: ")
+    if choice in ["1", "2", "3"]:
+        return scenario["options"][int(choice) - 1]
+    print("Please enter 1, 2, or 3.")
+    
+   
     pass
 def update_trait_scores (choice,scores):
     """Update personality trait scores."""
