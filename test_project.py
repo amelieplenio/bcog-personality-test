d080ca99e032868ee6ac6b5c36121e75cc5a815c
amelieplenio <aplenio2@illinois.edu>
2026-05-10 23:19:08 -0500

    Update test_project.py

--- test_project.py
+++ test_project.py
@@ -1,3 +1,22 @@
+from main import make_scores, update_trait_scores
+
+
+def test_make_scores():
+  scores = make_scores()
+  assert scores["openness"] == 0
+  assert scores["conscientiousness"] == 0
+  assert scores["extraversion"] == 0
+  assert scores["agreeableness"] == 0
+  assert scores["neuroticism"] == 0
+  
+def test_update_trait_scores():
+  scores = make_scores()
+  choice = {"traits": {"openness": 2, "extraversion": 1}}
+  updated_scores = update_trait_scores(choice, scores)
+
+  assert updated_scores ["openness"] == 2
+  assert updated_scores ["extraversion"] == 1
+  
 def test_program_runs():
   
     #Testing that the program runs without errors
