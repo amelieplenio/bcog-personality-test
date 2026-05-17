bb6aa0228c25a1abf57660af767f5ee2ed5da92b
amelieplenio <aplenio2@illinois.edu>
2026-05-16 21:51:50 -0500

    Update test_project.py

--- test_project.py
+++ test_project.py
@@ -1,8 +1,9 @@
-from main import make_scores, update_trait_scores
+from main import make_scores, update_trait_scores, get_highest_trait
 
 
 def test_make_scores():
   scores = make_scores()
+  
   assert scores["openness"] == 0
   assert scores["conscientiousness"] == 0
   assert scores["extraversion"] == 0
@@ -12,38 +13,42 @@ def test_make_scores():
 def test_update_trait_scores():
   scores = make_scores()
   choice = {"traits": {"openness": 2, "extraversion": 1}}
+  
   updated_scores = update_trait_scores(choice, scores)
 
   assert updated_scores ["openness"] == 2
   assert updated_scores ["extraversion"] == 1
   
-def test_program_runs():
+def test_multiple_updates():
+   scores = make_scores()
+
+   choice_one = {"traits": {"conscientiousness": 2}}
+   choice_one = {"traits": {"agreeableness": 2}}
+
+   update_trait_scores(choice_one,scores)
+   update_trait_scores(choice_two,scores)
+
+    assert scores["conscientiousness"] == 2
+    assert scores["agreeableness"] == 2
   
-    #Testing that the program runs without errors
+
+def test_get_highest_trait():
+
    
-    assert True
 
-def test_scenario_display():
-
-    #Testing that scenarios can be displayed to the user.
-
-    assert True
+    
 
 def test_trait_updates():
 
-    #Test that the choices correctly update personality scores.
+  
 
-    assert True
+   
 
-def test_final_result():
-
-    #Test that a final personality result is shown.
-
-    assert True
+  
 
     if __name__ =="__main__":
-        test_program_runs()
-        test_scenario_display()
-        test_trait_updates()
-        test_final_result()
-        print("All tests listed.")
+        test_make_scores()
+        test_update_trait_scores()
+        test_multiple_updates()
+        test_get_highest_trait()
+        print("All tests passed.")
